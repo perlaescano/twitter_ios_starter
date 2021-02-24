28978e935ffbaba248b9d0e4d83c7169e96cabea
Perla Escano <perlaescanoe@gmail.com>
2021-02-23 22:56:20 -0500

    login button test

---
+++ Twitter/loginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  loginViewController.swift
+//  Twitter
+//
+//  Created by Perla Escano Estrella on 2/23/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        print("hello world")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
