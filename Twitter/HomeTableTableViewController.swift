8f458e6a799c6001ee6b66a743e0ed73bf45f340
Perla Escano <perlaescanoe@gmail.com>
2021-02-25 22:10:11 -0500

    Logout

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Perla Escano Estrella on 2/25/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
